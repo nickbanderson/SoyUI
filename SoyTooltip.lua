705f179ce1f6caaf7ae4eee48b1e581650f30786
Nicholas Anderson <nick.b.anderson4@gmail.com>
2021-07-09 14:12:33 -0500

    soytooltip: prune, use module template, add ilvl to tooltip (sometimes)

--- SoyTooltip.lua
+++ SoyTooltip.lua
@@ -1,44 +1,29 @@
---[[
+local _, SoyUI = ...
 
-bug: show a tooltip, mouse over bonfire/object = wrong position
-	hooking OnUpdate is too late in the load cycle; it is UGLY
+local function addItemLevelToTooltips()
+	local function addItemLevel(tooltip, ...) 
+		local itemName, _ = tooltip:GetItem()
+		local ilvl, _, _ = GetDetailedItemLevelInfo(itemName);
+		if ilvl == nil then 
+			return end -- some items have no ilvl :S
 
-GameTooltip is a widget, so useful to look for widget handlers/methods
+		GameTooltipTextLeft1:SetText(
+			GameTooltipTextLeft1:GetText() .. ' (' .. ilvl .. ')'
+		)
+		-- tooltip:Show()
+	end
 
---]]
+	GameTooltip:HookScript('OnTooltipSetItem', addItemLevel) -- char, bag
+end
 
-local tooltip_x = -20
-local tooltip_y = 200
+local function init()
+	if SoyUI_DB.SoyTooltip.itemLevelOnTooltips then
+		addItemLevelToTooltips() end
+end
 
--- makes -first- load of ANY gametooltip positioned right (see bug)
-GameTooltip:HookScript("OnShow", function(self)
-	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", tooltip_x, tooltip_y)
-end)
-
--- players and npcs
-GameTooltip:HookScript("OnTooltipSetUnit", function(self)
-	--self:ClearAllPoints() -- not necessary until it is :)
-	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", tooltip_x, tooltip_y)
-
-	local name = GameTooltipTextLeft1:GetText()
-	local guild, _, _ = GetGuildInfo("mouseover")
-	local _, class, _ = UnitClass("mouseover")
-	if class == nil then class = "PRIEST" end -- priest is white (default text color)
-	if guild == nil then
-		guild = ""
-	else
-		guild = "\n|cffaaaaaa<" .. guild .. ">|r"
-  end
-
-	GameTooltipTextLeft1:SetText("|c" .. RAID_CLASS_COLORS[class].colorStr.. name .. "|r" .. guild)
-end)
-
--- action bar spells
-GameTooltip:HookScript("OnTooltipSetSpell", function(self)
-	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", tooltip_x, tooltip_x)
-end)
-
--- action bar items
-GameTooltip:HookScript("OnTooltipSetItem", function(self)
-	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", tooltip_x, tooltip_y)
-end)
+SoyUI.modules.SoyTooltip = {
+	init = init,
+	defaults = {
+		itemLevelOnTooltips = true,
+	},
+}
