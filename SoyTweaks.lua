cac6b605c97d47eb90a936ac99ee059f2430f6a2
Nick Anderson <nick.b.anderson4@gmail.com>
2022-03-07 19:49:48 -0600

    add nonfunctioning function to stop auto add spells

--- SoyTweaks.lua
+++ SoyTweaks.lua
@@ -128,6 +128,25 @@ local function enablePvpCountdowns()
   end)
 end
 
+local function disableAutoAddSpell()
+  -- This prevents icons from being animated onto the main action bar
+  IconIntroTracker.RegisterEvent = function() end
+  IconIntroTracker:UnregisterEvent('SPELL_PUSHED_TO_ACTIONBAR')
+
+  -- In the unlikely event that you're looking at a different action page while switching talents
+  -- the spell is automatically added to your main bar. This takes it back off.
+  local f = CreateFrame('frame')
+  f:SetScript('OnEvent', function(self, event, spellID, slotIndex, slotPos)
+	  -- This event should never fire in combat, but check anyway
+	  if not InCombatLockdown() then
+		  ClearCursor()
+		  PickupAction(slotIndex)
+		  ClearCursor()
+	  end
+  end)
+  f:RegisterEvent('SPELL_PUSHED_TO_ACTIONBAR')
+end
+
 function m.init()
   registerSlashReload()
   setCVars()
@@ -135,4 +154,5 @@ function m.init()
   if SoyUI_DB.SoyTweaks.showArenaNumberOnNameplate then
     -- enableArenaNumberOnNameplate()
   end
+  disableAutoAddSpell()
 end
